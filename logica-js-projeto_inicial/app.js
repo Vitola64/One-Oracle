5ee0792b94a152482bf2aad0635b1a1b9c9eadd6
Victor Henrique <victor.m.y@hotmail.com>
2024-06-20 11:42:58 -0300

    Numero secreto
    Iniciando lógica de programação na one oracle, fazendo jogo do numero secreto no js

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert('Bem vindo ao jogo do número secreto!');
+let numeroSecreto = 21;
+let chute = prompt('Escolha um número entre 1 e 30')
+
+if (numeroSecreto == chute) {
+    console.log('Parabéns, você acertou o número secreto!')
+}
