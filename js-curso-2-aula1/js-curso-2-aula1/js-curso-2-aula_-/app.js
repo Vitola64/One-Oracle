7dc4f44989e67789061736c71267404f8eabaae9
Victor Henrique <victor.m.y@hotmail.com>
2024-06-26 13:03:48 -0300

    correção
    corrigindo algumas coisas erradas e acrescentando lettitulo

--- js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,2 +1,19 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(`Número secreto: ${numeroSecreto}`)
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+//qual será a função do "verificarChute"? Verificar chute! (kk)
+function verificarChute() {
+    console.log('O botão foi clicado');
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
